a1c73924e087b58229f1922a40cb5fb41ac10c4b
Mitchell Pizzacalla <mtpizzacalla@gmail.com>
2021-02-04 17:15:29 +0000

    logical operators

--- conditionals.js
+++ conditionals.js
@@ -3,7 +3,7 @@ const temperature = 12;
 
 if (raining) {
 	console.log('Don\'t forget your umbrella!');
-}
+} else { console.log ('Leave your umbrella at home!'); }
 
 if (temperature < 0) {
 	console.log('Make sure you pick out a scarf!');
@@ -14,3 +14,15 @@ if (temperature < 0) {
 }
 
 console.log('Now you\'re ready to go outside!');
+
+// Logical operators
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+	console.log('You are eligible to vote.');
+}
+
+if (temperature < -40 || temperature > 40) {
+	console.log('Maybe going outside isn\'t such a great idea...');
+}
